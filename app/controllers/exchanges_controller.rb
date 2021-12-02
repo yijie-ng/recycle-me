6c2e1a742d2757bed916737acb4743196b85c565
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-12-02 20:00:36 +0800

    Removed new route and #new action from exchanges controller as exchanges form is embedded in items#show page, and added authorization to exchanges#create

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,21 +1,18 @@
 class ExchangesController < ApplicationController
-  before_action :set_item, only: [:new, :create, :update]
+  before_action :set_item, only: [:create, :update]
 
   def index
     @exchanges = policy_scope(Exchange).order(created_at: :desc)
   end
 
-  def new
-    @exchange = Exchange.new
-  end
-
   def create
     @exchange = Exchange.new(exchanges_params)
     @exchange.item = @item
+    @exchange.user = current_user
+    authorize @exchange
+
     if @exchange.save
       redirect_to item_path(@item)
-    else
-      render :new
     end
   end
 
