ebe6fd2f3776a886d002d402bbc78ad9476eb832
Alexander Noronha <alexander@noronha.asia>
2021-12-04 12:32:55 +0800

    exchanges view created to show user requests & user ids

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :items do
-    resources :exchanges, only: [:index, :create, :update]
+    resources :exchanges, only: [:create, :update]
   end
+  resources :exchanges, only: [:index]
 end
