55f5fc8a7ee8c3f1284bc271a52e44d7fa32f7df
Leslie Teo <lteo@ischool.berkeley.edu>
2021-12-06 14:25:44 +0800

    Added basic validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true
+  validates :title, length: { minimum: 5 }, presence: true
   validates :description, presence: true
-  validates :condition, presence: true
+  validates :condition, inclusion: { in: %w[Excellent Good Average Bad Broken] }, presence: true
 end
