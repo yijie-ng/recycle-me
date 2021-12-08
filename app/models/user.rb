8e259744f2f90a7875174ccc7fa8cad70e4ae5b3
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-12-08 21:42:49 +0800

    Added validates uniqueness for user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   has_many :items
   has_one_attached :photo
 
-  validates :name, length: { minimum: 3, maximum: 50 }, presence: true
+  validates :name, length: { minimum: 3, maximum: 50 }, uniqueness: true, presence: true
   validates :phone_number, length: { minimum: 8, maximum: 14 }, presence: true
 end
