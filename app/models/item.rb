2b91e82b6a286c076bfbed5dd416395727fb9723
Timothy Yeh <jcluvt@yahoo.com>
2021-12-08 21:11:40 +0800

    add pg search for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description,
+    against: [ :title, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
   belongs_to :user
   has_many_attached :photos
 
